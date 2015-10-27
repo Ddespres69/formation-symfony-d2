98495945eee493f2f4c3401edb4ea19a8741e290
formationProtoss <cristobal.protoss@gmail.com>
2015-10-27 16:03:12 +0100

    Enable/disable Computer Command +

--- src/ParkBundle/Command/ComputerToogleCommand.php
+++ src/ParkBundle/Command/ComputerToogleCommand.php
@@ -7,9 +7,8 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Security\Acl\Exception\Exception;
 
-class DisableComputerCommand extends ContainerAwareCommand
+class ComputerToogleCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
