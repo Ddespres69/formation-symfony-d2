f476fb60c7e802fd4c9391c96421dc6d5499a0c4
formationProtoss <cristobal.protoss@gmail.com>
2015-10-28 11:23:57 +0100

    PHPUNIT via composer

---
+++ src/ParkBundle/Service/Calculator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ParkBundle\Service;
+
+
+Class ComputerManager
+{
+
+
+    /**
+     * @param int $a
+     * @param int $b
+     * @return int
+     */
+    public function makeSum($a = 0, $b = 0)
+    {
+        return $a + $b;
+    }
+
+
+}
